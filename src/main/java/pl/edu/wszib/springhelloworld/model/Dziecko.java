ed45a5d14f5aee86412f613a46a31eaf04608adf
mamaleka <mamaleka@wszib.edu.pl>
2019-02-09 16:14:54 +0100

    autowired

--- src/main/java/pl/edu/wszib/springhelloworld/model/Dziecko.java
+++ src/main/java/pl/edu/wszib/springhelloworld/model/Dziecko.java
@@ -1,12 +1,31 @@
 package pl.edu.wszib.springhelloworld.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dziecko {
 
+   // @Autowired
     private Ciastko ciastko;
+  //  @Autowired
+    private Kot kot;
 
-    public Dziecko(Ciastko ciastko) {
+    @Autowired
+    public void setCiastko(Ciastko ciastko) {
         this.ciastko = ciastko;
     }
+
+    @Autowired
+    public void setKot(Kot kot) {
+        this.kot = kot;
+    }
+
+    /*    public Dziecko(Ciastko ciastko, Kot kot) {
+
+        this.ciastko = ciastko;
+        this.kot = kot;
+    }*/
 
     public void ugryz() {
         if(!this.ciastko.ugryzione()) {
